68b7851ac254fa72c78c95220980fc88916087c0
王莉萍 <5556929+wlp6897@user.noreply.gitee.com>
2022-03-26 19:09:03 +0800

    测试onBeforeRouteLeave

---
+++ src/views/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>首页啊</h1>
+    <router-link to="/test">去测试页面</router-link>
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/views/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>测试页面啊</h1>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeRouteLeave } from 'vue-router'
+onBeforeRouteLeave((to, from, next) => {
+  console.log(to)
+  console.log(from)
+  console.log("8888888")
+  setTimeout(() => {
+    next()
+  }, 1000)
+})
+</script>
